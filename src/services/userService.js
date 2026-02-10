b669d65c444787e037e5a34302364767e09a24d5
Anmol Mehla <anmolmehla1@outlook.com>
2026-02-10 12:49:59 +0530

    new updated in Backend APi

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8081/api/users";
+const BASE_URL = "https://userservices-latest.onrender.com";
 
 export const registerUser = async (user) => {
   const response = await fetch(`${BASE_URL}/register`, {
